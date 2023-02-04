fc986eed03d8f57dca917ef7c6760888e69ea2ba
altynd <altynd@mail.ru>
2023-02-04 06:49:53 +0000

    Changed the HTML pythonanywhere for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path(BASE_DIR / 'media')
