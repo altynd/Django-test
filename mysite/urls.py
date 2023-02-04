d88a7777dc3f573e62966f761e5c63777e83d111
Сэр Дъмитрий <altynd@mail.ru>
2023-02-04 17:53:12 +0400

    all working

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path("", include("blog.urls")),
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
